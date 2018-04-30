fee692fd4dda12113d011dd19e99df5914e31d85
santosh <santosh.ece06@gmail.com>
2018-04-30 15:29:03 +0530

    continue.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { Comp1Component } from './comp/comp1/comp1.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1Component
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [Comp1Component]
 })
 export class AppModule { }
