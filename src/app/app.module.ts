43921be73a5ca1a9edbee300bf2efc91b2f0a904
santosh <santosh.ece06@gmail.com>
2018-04-30 15:37:24 +0530

    a2 continue.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { Comp1Component } from './comp/comp1/comp1.component';
+import { Comp2Component } from './comp/comp2/comp2.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    Comp1Component
+    Comp2Component
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [Comp1Component]
+  bootstrap: [Comp2Component]
 })
 export class AppModule { }
