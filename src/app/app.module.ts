8352a895fc623964379f3da66027d396f11b06ac
santosh <santosh.ece06@gmail.com>
2018-05-06 12:28:06 +0530

    http module integration.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FlexLayoutModule} from '@angular/flex-layout';
-
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { Comp2Component } from './comp/comp2/comp2.component';
@@ -13,7 +13,9 @@ import { Comp2Component } from './comp/comp2/comp2.component';
     Comp2Component
   ],
   imports: [
-    BrowserModule, FlexLayoutModule
+    BrowserModule,
+    HttpClientModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [Comp2Component]
